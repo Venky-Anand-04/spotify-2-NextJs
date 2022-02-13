63e145af2364cf5ea103ce23c459d82fe57123d2
Venkatesh Anandan <venkatesha04offcl@gmail.com>
2022-02-13 19:29:22 +0530

    removed the url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -32,7 +32,7 @@ export default NextAuth({
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-      authorization: process.env.LOGIN_URL,
+      authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
